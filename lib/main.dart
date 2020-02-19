110b55a92ab1acf1c907f3486cf289c3953a0d37
codeclab <kabanofor@yahoo.com>
2020-02-19 23:48:08 +0300

    attempt at splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -142,3 +142,37 @@ class DisplayPictureScreen extends StatelessWidget {
     );
   }
 }
+
+class SplashScreen extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return SplashScreenState();
+  }
+}
+
+class SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/splash.jpg'),
+            fit: BoxFit.cover
+        ) ,
+      ),
+      child: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.redAccent),
+        ),
+      ),
+    );
+  }
+}
