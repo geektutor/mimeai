9b1f863f7b22a61446a3974e62d09c26b1f6fbdb
lordlamee <gabrielolamideakorede@gmail.com>
2020-07-17 12:19:22 +0100

    camera screen

--- lib/camera.dart
+++ lib/camera.dart
@@ -75,162 +75,321 @@ class TakePictureScreenState extends State<TakePictureGeek> {
   Widget build(BuildContext context) {
     screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      // Wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner
-      // until the controller has finished initializing.
-      body: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 40),
-              Container(
-                width: 350,
-                height: 152,
-                padding: EdgeInsets.all(30),
-                margin: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Color(0xFFFFF7F1),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'mimeai',
-                      style: GoogleFonts.nunito(
-                        textStyle: TextStyle(fontSize: 20),
-                        letterSpacing: 0.03,
-                        color: Colors.green,
-                      ),
+        // Wait until the controller is initialized before displaying the
+        // camera preview. Use a FutureBuilder to display a loading spinner
+        // until the controller has finished initializing.
+        body: SafeArea(
+      child: Stack(
+        children: <Widget>[
+          Container(
+              height: screenSize.height,
+              width: screenSize.width,
+              //   padding: EdgeInsets.symmetric(horizontal: 30),
+              child: FutureBuilder<void>(
+                future: _initializeControllerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    // If the Future is complete, display the preview.
+                    return CameraPreview(_controller);
+                  } else {
+                    // Otherwise, display a loading indicator.
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
+              )),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              height: screenSize.height * 0.25,
+              width: screenSize.width,
+              // color: Colors.grey.withOpacity(0.3),
+              child: ClipRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 5,
+                    sigmaY: 5,
+                  ),
+                  child: Container(
+                    height: screenSize.height * 0.25 - 70,
+                    width: screenSize.width - 40,
+                    padding: EdgeInsets.fromLTRB(24, 24, 0, 24),
+                    margin: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xFFFFF7F1),
                     ),
-                    Text(
-                      'Take a picture of an affected leaf of the plant',
-                      style: GoogleFonts.nunito(
-                        textStyle: TextStyle(fontSize: 18),
-                        letterSpacing: 0.03,
-                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        mimeai,
+                        Text(
+                          'Take a picture of an affected  leaf of the plant',
+                          style: GoogleFonts.manrope(
+                            textStyle: TextStyle(),
+                            fontSize: 16,
+                            letterSpacing: 0.03,
+
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-              Container(
-//                    height: 300,
-//                    width: 300,
-              padding: EdgeInsets.symmetric(horizontal: 30),
-                   child: Column(
-                     children: <Widget>[
-                       FutureBuilder<void>(
-                         future: _initializeControllerFuture,
-                         builder: (context, snapshot) {
-                           if (snapshot.connectionState ==
-                               ConnectionState.done) {
-                             // If the Future is complete, display the preview.
-                             return Container(
-                               width: screenSize.width,
-                               height: screenSize.width -60,
-                               child: CameraPreview(_controller),
-                             );
-                           } else {
-                             // Otherwise, display a loading indicator.
-                             return Center(child: CircularProgressIndicator());
-                           }
-                         },
-                       ),
-                     ],
-                   )),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 64,
-                    height: 64,
-                    child: FloatingActionButton(
-                      heroTag: "btn1",
-                      backgroundColor: Color(0xFF569557),
-                      onPressed: null,
-                      child: Icon(
-                        FeatherIcons.zap,
-                        size: 35,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: FloatingActionButton(
-                      heroTag: "btn2",
-                      backgroundColor: Color(0xFF569557),
-                      child: Icon(
-                        FeatherIcons.camera,
-                        size: 35,
-                      ),
-                      onPressed: () async {
-                        // Take the Picture in a try / catch block. If anything goes wrong,
-                        // catch the error.
-                        try {
-                          // Ensure that the camera is initialized.
-                          await _initializeControllerFuture;
-
-                          // Construct the path where the image should be saved using the
-                          // pattern package.
-                          final path = join(
-                            // Store the picture in the temp directory.
-                            // Find the temp directory using the `path_provider` plugin.
-                            (await getTemporaryDirectory()).path,
-                            '${DateTime.now()}.png',
-                          );
-
-                          // Attempt to take a picture and log where it's been saved.
-                          await _controller.takePicture(path);
-
-                          // If the picture was taken, display it on a new screen.
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Analyzing(
-                                imagePath: path,
-                              ),
-                            ),
-                          );
-                        } catch (e) {
-                          // If an error occurs, log the error to the console.
-                          print(e);
-                        }
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Container(
-                    width: 64,
-                    height: 64,
-                    child: FloatingActionButton(
-                      heroTag: "btn3",
-                      backgroundColor: Color(0xFF569557),
-                      onPressed: null,
-                      child: Icon(
-                        FeatherIcons.refreshCw,
-                        size: 35,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: screenSize.height * 0.22,
+              width: screenSize.width,
+              child: ClipRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 5,
+                    sigmaY: 5,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 64,
+                        height: 64,
+                        child: FloatingActionButton(
+                          heroTag: "btn1",
+                          backgroundColor: Color(0xFF569557),
+                          onPressed: null,
+                          child: Icon(
+                            FeatherIcons.zap,
+                            size: 35,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        child: FloatingActionButton(
+                          heroTag: "btn2",
+                          backgroundColor: Color(0xFF569557),
+                          child: Icon(
+                            FeatherIcons.camera,
+                            size: 35,
+                          ),
+                          onPressed: () async {
+// Take the Picture in a try / catch block. If anything goes wrong,
+// catch the error.
+                            try {
+// Ensure that the camera is initialized.
+                              await _initializeControllerFuture;
+
+// Construct the path where the image should be saved using the
+// pattern package.
+                              final path = join(
+// Store the picture in the temp directory.
+// Find the temp directory using the `path_provider` plugin.
+                                (await getTemporaryDirectory()).path,
+                                '${DateTime.now()}.png',
+                              );
+
+// Attempt to take a picture and log where it's been saved.
+                              await _controller.takePicture(path);
+
+// If the picture was taken, display it on a new screen.
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Analyzing(
+                                    imagePath: path,
+                                  ),
+                                ),
+                              );
+                            } catch (e) {
+// If an error occurs, log the error to the console.
+                              print(e);
+                            }
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        width: 64,
+                        height: 64,
+                        child: FloatingActionButton(
+                          heroTag: "btn3",
+                          backgroundColor: Color(0xFF569557),
+                          onPressed: null,
+                          child: Icon(
+                            FeatherIcons.refreshCw,
+                            size: 35,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              //color: Colors.grey.withOpacity(0.3),
+            ),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
 
+//
+//Center(
+//child: Column(
+//crossAxisAlignment: CrossAxisAlignment.center,
+//children: <Widget>[
+//SizedBox(height: 40),
+//Container(
+//width: 350,
+//height: 152,
+//padding: EdgeInsets.all(30),
+//margin: EdgeInsets.all(20),
+//decoration: BoxDecoration(
+//borderRadius: BorderRadius.circular(20),
+//color: Color(0xFFFFF7F1),
+//),
+//child: Column(
+//crossAxisAlignment: CrossAxisAlignment.start,
+//children: <Widget>[
+//Text(
+//'mimeai',
+//style: GoogleFonts.nunito(
+//textStyle: TextStyle(fontSize: 20),
+//letterSpacing: 0.03,
+//color: Colors.green,
+//),
+//),
+//Text(
+//'Take a picture of an affected leaf of the plant',
+//style: GoogleFonts.nunito(
+//textStyle: TextStyle(fontSize: 18),
+//letterSpacing: 0.03,
+//),
+//),
+//],
+//),
+//),
+//Container(
+////                    height: 300,
+////                    width: 300,
+//padding: EdgeInsets.symmetric(horizontal: 30),
+//child: Column(
+//children: <Widget>[
+//FutureBuilder<void>(
+//future: _initializeControllerFuture,
+//builder: (context, snapshot) {
+//if (snapshot.connectionState ==
+//ConnectionState.done) {
+//// If the Future is complete, display the preview.
+//return Container(
+//width: screenSize.width,
+//height: screenSize.width -60,
+//child: CameraPreview(_controller),
+//);
+//} else {
+//// Otherwise, display a loading indicator.
+//return Center(child: CircularProgressIndicator());
+//}
+//},
+//),
+//],
+//)),
+//SizedBox(height: 10),
+//Row(
+//mainAxisAlignment: MainAxisAlignment.center,
+//children: <Widget>[
+//Container(
+//width: 64,
+//height: 64,
+//child: FloatingActionButton(
+//heroTag: "btn1",
+//backgroundColor: Color(0xFF569557),
+//onPressed: null,
+//child: Icon(
+//FeatherIcons.zap,
+//size: 35,
+//),
+//),
+//),
+//SizedBox(
+//width: 20,
+//),
+//Container(
+//width: 100,
+//height: 100,
+//child: FloatingActionButton(
+//heroTag: "btn2",
+//backgroundColor: Color(0xFF569557),
+//child: Icon(
+//FeatherIcons.camera,
+//size: 35,
+//),
+//onPressed: () async {
+//// Take the Picture in a try / catch block. If anything goes wrong,
+//// catch the error.
+//try {
+//// Ensure that the camera is initialized.
+//await _initializeControllerFuture;
+//
+//// Construct the path where the image should be saved using the
+//// pattern package.
+//final path = join(
+//// Store the picture in the temp directory.
+//// Find the temp directory using the `path_provider` plugin.
+//(await getTemporaryDirectory()).path,
+//'${DateTime.now()}.png',
+//);
+//
+//// Attempt to take a picture and log where it's been saved.
+//await _controller.takePicture(path);
+//
+//// If the picture was taken, display it on a new screen.
+//Navigator.pushReplacement(
+//context,
+//MaterialPageRoute(
+//builder: (context) => Analyzing(
+//imagePath: path,
+//),
+//),
+//);
+//} catch (e) {
+//// If an error occurs, log the error to the console.
+//print(e);
+//}
+//},
+//),
+//),
+//SizedBox(
+//width: 20,
+//),
+//Container(
+//width: 64,
+//height: 64,
+//child: FloatingActionButton(
+//heroTag: "btn3",
+//backgroundColor: Color(0xFF569557),
+//onPressed: null,
+//child: Icon(
+//FeatherIcons.refreshCw,
+//size: 35,
+//),
+//),
+//),
+//],
+//),
+//],
+//),
+//),
 // A widget that displays the picture taken by the user.
 class DisplayPictureGeek extends StatelessWidget {
   final String imagePath;
